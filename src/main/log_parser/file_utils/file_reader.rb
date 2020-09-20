0f50d4874f15e5ec928fb729c925063fd27b0db0
Pedro de Jesus <pedro.jesus@talkdesk.com>
2020-09-20 20:58:10 +0100

    fixup! Add FileReader class to read the input log file

--- src/main/log_parser/file_utils/file_reader.rb
+++ src/main/log_parser/file_utils/file_reader.rb
@@ -4,8 +4,10 @@ module LogParser
   module FileUtils
     class FileReader
       def read(file_path)
-        File.open(file_path, 'r').each_line do |line|
-          yield line.gsub("\n", '')
+        File.open(file_path, 'r') do |file|
+          file.each_line do |line|
+            yield line.gsub("\n", '')
+          end
         end
       end
     end
