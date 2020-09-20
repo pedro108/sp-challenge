05aea759b5126a12966ee86b505f44e0adc64c1a
Pedro de Jesus <pedro.jesus@talkdesk.com>
2020-09-20 20:59:06 +0100

    Change main to validate the input log_file_path

--- src/main/log_parser/main.rb
+++ src/main/log_parser/main.rb
@@ -1,21 +1,26 @@
 require 'log_parser/file_utils/file_reader'
+require 'log_parser/validator/log_file_path_validator'
 
 module LogParser
   class Main
     def self.parse(log_file_path:)
-      new(file_reader: LogParser::FileUtils::FileReader.new).parse(log_file_path)
+      new.parse(log_file_path)
     end
 
-    def initialize(file_reader:)
+    def initialize(file_reader: LogParser::FileUtils::FileReader.new,
+                   log_file_path_validator: LogParser::Validator::LogFilePathValidator.new
+    )
       @file_reader = file_reader
+      @log_file_path_validator = log_file_path_validator
     end
 
     def parse(log_file_path)
+      log_file_path_validator.validate(log_file_path)
       log_file_path
     end
 
     private
 
-    attr_reader :file_reader
+    attr_reader :file_reader, :log_file_path_validator
   end
 end
--- src/spec/log_parser/main_spec.rb
+++ src/spec/log_parser/main_spec.rb
@@ -1,13 +1,40 @@
 # frozen_string_literal: true
 
+require 'fakefs_helper'
 require 'log_parser/main'
 
 RSpec.describe LogParser::Main do
-  it 'returns the received log file_utils path' do
-    file_path = 'path/to/log/file_utils'
+  include FakeFS::SpecHelpers
 
-    parsed_file_path = LogParser::Main.parse(log_file_path: file_path)
+  context 'when the file is parsed successfully' do
+    it 'returns the received log file_utils path' do
+      FakeFS.with_fresh do
+        file_path = 'logfile.log'
+        content = "/help_page/1 126.318.035.038\n/contact 184.123.665.067"
+        File.write(file_path, content, mode: 'w')
 
-    expect(parsed_file_path).to eql(file_path)
+        parsed_file_path = LogParser::Main.parse(log_file_path: file_path)
+
+        expect(parsed_file_path).to eql(file_path)
+      end
+    end
+  end
+
+  context 'when no log_file_path parameter is passed' do
+    it 'raises an error with a log_file_path parameter is required message' do
+      expect {
+        LogParser::Main.parse(log_file_path: nil)
+      }.to raise_error(RuntimeError, 'A log_file_path parameter is required. e.g. $ log-parser path/to/webserver.log')
+    end
+  end
+
+  context 'when the file pointed by given log_file_path parameter does not exist' do
+    it 'raises an error with a log_file not found message' do
+      FakeFS.with_fresh do
+        expect {
+          LogParser::Main.parse(log_file_path: 'path/to/non_existent_file.log')
+        }.to raise_error(RuntimeError, 'No log file found for path path/to/non_existent_file.log')
+      end
+    end
   end
 end
